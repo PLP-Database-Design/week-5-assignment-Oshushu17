bbff7e18d824ec8a5b952213a96a3c0ba1a0e46f
Oshushu17 <116732990+Oshushu17@users.noreply.github.com>
2024-10-03 14:37:26 +0200

    Answers to week 5 assignment.
    Sir i couldn't clone the repo without error coming up. That was why i submitted via file upload. Pardon me Sir.  I also included ejs files.

---
+++ server.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const mysql = require('mysql2')
+
+
+
+
+//setting up database 
+const db = mysql.createConnection({
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME,
+    port: process.env.DB_PORT
+})
+
+//checking database connection setup
+db.connect((err)=>{
+    if(err){
+        console.log("Error connecting to DB", threadId)
+    }
+    console.log(`Backend running on port: ${port} and id: ${db.threadId}`)
+   
+})
+
+
+const app = express()
+app.use(express.json());
+
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname +'/views')
+
+
+
+// question 1
+// all patients endpoint
+app.get('/patient', (req, res)=>{
+    db.query('SELECT * FROM patients', (err, results)=>{
+        if (err){
+            console.error(err)
+            res.status(500).send('Error populating data from DB')
+        }else{
+            res.render('patients', {results:results})
+        }
+    });
+});
+
+//question 2
+//patient's by first name
+app.get('/patient/firstname', (req, res)=>{
+    db.query('SELECT first_name FROM patients', (err, results)=>{
+        if (err){
+            console.error(err)
+            res.status(500).send('Error populating data from DB')
+        }else{
+            res.render('patients_firstName', {results:results})
+        }
+    });
+});
+
+//question 3
+//provider's endpoint
+app.get('/provider', (req, res)=>{
+    db.query('SELECT * FROM providers', (err, results)=>{
+        if(err){
+            console.error(err)
+            res.status(500).send(("Error retrieving provider's details"))
+        }else{
+            res.render('providers', {results:results})
+        }
+    });
+});
+
+// question 4
+//provider's specialty endpoint
+app.get('/provider/specialty', (req,res)=>{
+    db.query('SELECT provider_specialty FROM providers', (err, results)=>{
+        if (err){
+            console.error(err)
+            res.status(500).send('Error retrieving provider data')
+        }else{
+            res.render('providers_specialty', {results:results})
+            
+        }
+    });
+});
+
+
+const port = 3000
+app.listen(port, ()=>{
+    console.log(`Server listening on port: ${port}`)
+    console.log('Sending to browser...')
+})
